50f2f4d9162d284497c4e052b0af820d89c48f6e
Joseph Freedman <jfreedman340@gmail.com>
2022-10-19 11:57:37 -0700

    I made the Blink class and experimented with programming LED lights.

---
+++ src/Blink.java
@@ -0,0 +1,77 @@
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import org.zeromq.SocketType;
+import org.zeromq.ZMQ;
+import org.zeromq.ZContext;
+import java.util.concurrent.TimeUnit;
+
+public class Blink {
+    private ZContext zctx;
+    private ZMQ.Socket zsocket;
+    private Gson gson;
+    private String connStr;
+    private final String topic = "GPIO";
+
+    private static final int[] OFF = {0, 0, 0};
+
+    public Blink(String protocol, String host, int port) {
+        zctx = new ZContext();
+        zsocket = zctx.createSocket(SocketType.PUB);
+        this.connStr = String.format("%s://%s:%d", protocol, "*", port);
+        zsocket.bind(connStr);
+        this.gson = new Gson();
+    }
+
+    public void send(int[] color) throws InterruptedException {
+        JsonArray ja = gson.toJsonTree(color).getAsJsonArray();
+        String message = topic + " " + ja.toString();
+        System.out.println(message);
+        zsocket.send(message);
+    }
+
+    public void blinkN(int[] color, int times, int miliseconds) throws  InterruptedException{
+        for(int i=0; i<times; i++) {
+            send(color);
+            TimeUnit.MILLISECONDS.sleep(miliseconds);
+            send(Blink.OFF);
+            TimeUnit.MILLISECONDS.sleep(miliseconds);
+        }
+    }
+
+    public void close() throws InterruptedException {
+        TimeUnit.SECONDS.sleep(2); // Allow the socket a chance to flush.
+        this.zsocket.close();
+        this.zctx.close();
+    }
+
+    public static void main(String[] args) {
+        LEDClient ledClient = new LEDClient("tcp", "192.168.86.250", 5001);
+        try {
+            int[] blue = {51,204,255};
+            int[] red = {255, 0 , 0};
+            int[] yellow = {255, 204, 0};
+            int[] green = {0, 153, 0};
+            int[] purple = {102, 0, 153};
+            int[] orange = {255, 102, 0};
+            int[] grey = {153, 153, 153};
+            int[] brown = {102, 51, 0};
+
+            ledClient.blinkN(blue, 1, 1000);
+            ledClient.blinkN(red, 2, 500);
+            ledClient.blinkN(yellow, 3, 250);
+            ledClient.blinkN(green, 4, 125);
+            ledClient.blinkN(yellow, 5, 120);
+            ledClient.blinkN(red, 6, 115);
+            ledClient.blinkN(blue, 7, 110);
+
+            ledClient.blinkN(brown, 8, 100);
+            ledClient.blinkN(purple, 9, 90);
+            ledClient.blinkN(orange, 10, 80);
+            ledClient.blinkN(grey, 11, 70);
+
+            ledClient.close();
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
